a1f59a23d06d13d0ad133d7aafd6aa2014fe05b1
Patrik Cmil <cmil.patrik@gmail.com>
2026-02-28 13:11:49 +0100

    Initial commit: Vite + React + Tailwind 4

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-5xl font-extrabold text-red-600 underline">
+        Tailwind Funguje!
+      </h1>
+    </>
+  )
+}
+
+export default App
