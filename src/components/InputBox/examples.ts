e6b02011149b14d4ce0367d5c71ac50f4e532be9
Patrik Cmil <cmil.patrik@gmail.com>
2026-03-19 10:33:04 +0100

    add example 1

--- src/components/InputBox/examples.ts
+++ src/components/InputBox/examples.ts
@@ -1,7 +1,18 @@
 // examples.ts
 // Mapovanie názvov príkladov na ich obsah
 export const EXAMPLES: { label: string; value: string }[] = [
-  { label: "Príklad 1", value: "∀x ∃y P(x, y)" },
+  { 
+    label: "Príklad 1", 
+    value: `parent(jozo, jano).
+parent(jozo, erik). 
+parent(jozo, maria).
+parent(maria, kika).
+not_same(jano, maria).
+not_same(jano, erik).
+grandparent(X, Y) :- parent(X, Z), parent(Z, Y).
+sibling(X, Y) :- parent(P, X), parent(P, Y), not_same(X, Y).
+?- sibling(jano, S).` 
+  },
   { label: "Príklad 2", value: "∃x (Q(x) ∧ R(x))" },
   { label: "Príklad 3", value: "∀x (P(x) => Q(x))" },
   { label: "Príklad 4", value: "((∀x)(C(x)=>S(x))∧(∃y)(C(y)∧P(y)))∧¬S(Punto)" },
